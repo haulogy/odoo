612a3362c76156d86112c19115cb0ee3f17da671
Thomas Fossoul <thomas.fossoul@haulogy.net>
2021-11-04 14:44:54 +0100

    Update max_age of session cookies to 8h instead of 3 months

--- odoo/http.py
+++ odoo/http.py
@@ -1398,7 +1398,7 @@ class Root(object):
         # - It could allow session fixation attacks.
         if not explicit_session and hasattr(response, 'set_cookie'):
             response.set_cookie(
-                'session_id', httprequest.session.sid, max_age=90 * 24 * 60 * 60, httponly=True)
+                'session_id', httprequest.session.sid, max_age=8 * 60 * 60, httponly=True)
 
         return response
 
